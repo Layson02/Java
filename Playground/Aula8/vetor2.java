525bead047fefaeb905c71eef981ba4d3baba9b8
laysoniesp <232889792+laysoniesp@users.noreply.github.com>
2026-05-11 15:34:01 -0300

    feat: Add vetor2 class with main method and array initialization
    feat: Adicionar a classe vetor2 com método main e inicialização de array

---
+++ Playground/Aula8/vetor2.java
@@ -0,0 +1,9 @@
+public class vetor2 {
+    public static void main(String[] args) {
+        int[] vetor = new int[]{10, 20, 30, 40, 50};
+        
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("vetor[" + i + "] = " + vetor[i]);
+        }
+    }
+}
