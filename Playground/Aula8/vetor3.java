c4f3ef9af9f5c3c8fa89d48917feaa8f310b6f6a
laysoniesp <232889792+laysoniesp@users.noreply.github.com>
2026-05-11 16:02:12 -0300

    feat: Update vetor3 class to use a string array for color names
    feat: Atualizar a classe vetor3 para usar um array de strings para nomes de cores

--- Playground/Aula8/vetor3.java
+++ Playground/Aula8/vetor3.java
@@ -1,8 +1,8 @@
 public class vetor3 {
     public static void main(String[] args) {
-        int vetor[] = {10, 20, 30, 40, 50};
-        for (int elemento : vetor) {
-            System.out.println(elemento);
+        String vetor[] = {"Vermelho", "Verde", "Azul", "Amarelo"};
+        for (String cores : vetor) {
+            System.out.println(cores);
         }
     }
 }
