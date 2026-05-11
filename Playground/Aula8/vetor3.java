51022c253a48c358c326cc3201bdf36816b6084d
laysoniesp <232889792+laysoniesp@users.noreply.github.com>
2026-05-11 15:52:58 -0300

    feat: Add vetor3 class with main method and array initialization
    feat: Adicionar a classe vetor3 com método main e inicialização de array

---
+++ Playground/Aula8/vetor3.java
@@ -0,0 +1,8 @@
+public class vetor3 {
+    public static void main(String[] args) {
+        int vetor[] = {10, 20, 30, 40, 50};
+        for (int elemento : vetor) {
+            System.out.println(elemento);
+        }
+    }
+}
